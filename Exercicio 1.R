7f862d05aa9e1a11cb4be1b04c2bd395739caa75
Renato <renatomarquesteles@gmail.com>
2020-08-16 18:43:17 -0300

    Exercicio 1

---
+++ Exercicio 1.R
@@ -0,0 +1,85 @@
+# RENATO MARQUES TELES, 21321
+# EXERCICIO 1
+
+# 1
+x = 1
+y = 2
+x + y
+x - y
+x * y
+x / y
+x %/% y
+x %% y
+
+# 2
+sin(pi / 6)
+cos(pi / 6)
+tan(pi / 6)
+
+# 3
+## a
+z = seq(1, 40, 2)
+## b
+sum(z)
+## c
+w = which(z %% 3 == 0)
+z[w]
+## d
+z[w] / 3
+
+# 4
+sum(seq(2, 1000, 2))
+
+# 5
+v = sqrt(1 : 20)
+v
+
+# 6
+## a
+p = 1 : 20
+## b
+p + rev(p)
+## c
+p + 3
+## d
+1 / p
+
+# 7
+## a
+az = "azul"
+az
+## b
+am = "amarelo"
+## c
+paste(az, am)
+
+# 8
+nome = "renato"
+sub("r", "R", nome)
+
+# 9
+numeros = c(1, 2, 3)
+nomes = c("Ana", "João", "Maria")
+notas = c(9, 4, 7)
+paste(numeros, nomes, notas, sep = " - ")
+
+# 10
+gastos = c(60, 55, 38, 87, 65, 63, 43, 44, 45, 50, 78, 67)
+## a
+sum(gastos)
+## b
+min(gastos)
+max(gastos)
+which.min(gastos)
+which.max(gastos)
+## c
+length(which(gastos > mean(gastos)))
+x = which(gastos > mean(gastos))
+y = gastos[gastos > mean(gastos)] / sum(gastos)
+paste(x, y, sep = ": ")
+
+# 11
+## a
+p = round(runif(100, 1, 100))
+## b
+niveis = c("Mau", "Insuficiente", "Bom", "Muito Bom")
